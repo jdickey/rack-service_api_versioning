7ac549df1c459efaefb3fe2b68d0e8d59e4ff201
Jeff Dickey <jdickey@seven-sigma.com>
2017-03-01 01:44:14 +0800

    fix(middleware): Main include file includes all three main middleware components
    Previously, `api_version_redirector` had been inadvertently omitted.
    
    When addressing Issue #5, note that the new “integration” test here references the `ApiVersionRedirector` class without namespacing, and will need to be fixed at that time.
    
    [Closes #8]

---
+++ test/rack/service_api_versioning/integration/all_middleware_included_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+require 'rack/service_api_versioning'
+
+describe 'ServiceApiVersioning middleware has' do
+  it 'an AcceptContentTypeSelector class' do
+    actual = Rack::ServiceApiVersioning::AcceptContentTypeSelector
+    expect(actual).must_be_instance_of Class
+  end
+
+  it 'an ApiVersionRedirector class' do
+    actual = ApiVersionRedirector # NOTE: Issue #5
+    expect(actual).must_be_instance_of Class
+  end
+
+  it 'a ServiceComponentDescriber class' do
+    actual = Rack::ServiceApiVersioning::ServiceComponentDescriber
+    expect(actual).must_be_instance_of Class
+  end
+end # describe 'ServiceApiVersioning middleware has'
